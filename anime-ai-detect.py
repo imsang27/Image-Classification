27d741f8e4d176ba61ff436b4ee437a82cff6a7d
imsang27 <69525587+imsang27@users.noreply.github.com>
2024-12-04 04:45:17 +0900

    Base Example Code

--- anime-ai-detect.py
+++ anime-ai-detect.py
@@ -2,6 +2,7 @@
 
 # Use a pipeline as a high-level helper
 from transformers import pipeline
+from PIL import Image
 
 pipe = pipeline("image-classification", model="saltacc/anime-ai-detect")
 
@@ -10,3 +11,13 @@ from transformers import AutoImageProcessor, AutoModelForImageClassification
 
 processor = AutoImageProcessor.from_pretrained("saltacc/anime-ai-detect")
 model = AutoModelForImageClassification.from_pretrained("saltacc/anime-ai-detect")
+
+# 분류 파이프라인 로드
+pipe = pipeline("image-classification", model="saltacc/anime-ai-detect")
+
+# 분류할 이미지 로드
+image = Image.open("example.jpg")
+
+# 결과 예측
+result = pipe(image)
+print(result)
